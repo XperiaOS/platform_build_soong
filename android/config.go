LineageOS/android_build_soong

soong: Add java sources overlay support
Change-Id: I94143febb0a8afa6a165364d36a40d5120a4e7bc

android/config.go
@@ -27,6 +27,8 @@ import (
 
 	"github.com/google/blueprint/bootstrap"
 	"github.com/google/blueprint/proptools"
+
+	"lineage/soong/android"
 )
 
 var Bool = proptools.Bool
@@ -466,6 +468,14 @@ func (c *config) ResourceOverlays() []string {
 		return nil
 	}
 	return *c.productVariables.ResourceOverlays
+}
+
+func (c *config) JavaSourceOverlays() string {
+	return String(c.productVariables.Lineage.Java_Source_Overlays)
+}
+
+func (c *config) JavaSourceOverlayModuleWhitelist() []string {
+	return android.LineageConfig.JavaSourcesOverlayModuleWhitelist
 }
 
 func (c *config) PlatformVersionName() string {

XperiaOS/platform_build_soong

Signed-off-by: Thespartann <danilastefan69@yahoo.ro>

android/config.go
@@ -27,6 +27,8 @@ import (
 
 	"github.com/google/blueprint/bootstrap"
 	"github.com/google/blueprint/proptools"
+
+	"xperia/soong/android"
 )
 
 var Bool = proptools.Bool
@@ -463,6 +465,14 @@ func (c *config) ResourceOverlays() []string {
 		return nil
 	}
 	return *c.productVariables.ResourceOverlays
+}
+
+func (c *config) JavaSourceOverlays() string {
+	return String(c.productVariables.Xperia.Java_Source_Overlays)
+}
+
+func (c *config) JavaSourceOverlayModuleWhitelist() []string {
+	return android.XperiaConfig.JavaSourceOverlayModuleWhitelist
 }
 
 func (c *config) PlatformVersionName() string {